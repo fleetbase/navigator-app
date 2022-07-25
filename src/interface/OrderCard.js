eb569b6d23ba87a882f6bcf243d7272c23c8a0ee
Ronald A. Richardson <me@ron.dev>
2022-07-25 17:51:30 +0800

    feature/ update to display nearby adhoc orders to driver

--- src/interface/OrderCard.js
+++ src/interface/OrderCard.js
@@ -6,30 +6,47 @@ import { formatDuration, formatKm } from 'utils';
 import OrderStatusBadge from './OrderStatusBadge';
 import OrderWaypoints from './OrderWaypoints';
 
-const OrderCard = ({ order, onPress, wrapperStyle, containerStyle, headerStyle, waypointsContainerStyle }) => {
+const OrderCard = ({
+    order,
+    onPress,
+    wrapperStyle,
+    containerStyle,
+    headerStyle,
+    waypointsContainerStyle,
+    badgeWrapperStyle,
+    textStyle,
+    orderIdStyle,
+    dateStyle,
+    timeStyle,
+    distanceStyle,
+    headerTop = null,
+    headerBottom = null,
+}) => {
     const scheduledAt = order.scheduledAt ? format(order.scheduledAt, 'PPpp') : null;
     const createdAt = order.createdAt ? format(order.createdAt, 'PPpp') : null;
 
     return (
         <View style={[tailwind('p-2'), wrapperStyle]}>
             <TouchableOpacity style={[tailwind('bg-gray-800 border border-gray-700 rounded-md shadow-sm w-full'), containerStyle]} onPress={onPress}>
+                {headerTop}
                 <View style={[tailwind('border-b border-gray-700 py-3 px-3 flex flex-row items-start justify-between'), headerStyle]}>
-                    <View style={tailwind('flex flex-col')}>
-                        <Text style={tailwind('text-white font-semibold mb-1')}>{order.id}</Text>
-                        <Text style={tailwind('text-gray-50 mb-1')}>{scheduledAt ?? createdAt}</Text>
-                        <View style={tailwind('flex flex-row')}>
-                            <Text style={tailwind('text-gray-100')}>{formatDuration(order.getAttribute('time'))}</Text>
-                            <Text style={tailwind('text-gray-100 mx-1')}>•</Text>
-                            <Text style={tailwind('text-gray-100')}>{formatKm(order.getAttribute('distance') / 1000)}</Text>
+                    <View style={[tailwind('flex flex-col')]}>
+                        <Text style={[tailwind('text-white font-semibold mb-1'), textStyle, orderIdStyle]}>{order.id}</Text>
+                        <Text style={[tailwind('text-gray-50 mb-1'), textStyle, dateStyle]}>{scheduledAt ?? createdAt}</Text>
+                        <View style={[tailwind('flex flex-row'), textStyle]}>
+                            <Text style={[tailwind('text-gray-100'), textStyle, timeStyle]}>{formatDuration(order.getAttribute('time'))}</Text>
+                            <Text style={[tailwind('text-gray-100 mx-1'), textStyle]}>•</Text>
+                            <Text style={[tailwind('text-gray-100'), textStyle, distanceStyle]}>{formatKm(order.getAttribute('distance') / 1000)}</Text>
                         </View>
+                        {headerBottom}
                     </View>
-                    <View style={tailwind('flex flex-col items-end justify-start')}>
+                    <View style={[tailwind('flex flex-col items-end justify-start'), badgeWrapperStyle]}>
                         <OrderStatusBadge status={order.getAttribute('status')} />
                         {order.getAttribute('status') === 'created' && order.isDispatched && <OrderStatusBadge status={'dispatched'} wrapperStyle={tailwind('mt-1')} />}
                     </View>
                 </View>
                 <View style={[tailwind('p-4'), waypointsContainerStyle]}>
-                    <OrderWaypoints order={order} />
+                    <OrderWaypoints order={order} textStyle={textStyle} />
                 </View>
             </TouchableOpacity>
         </View>
