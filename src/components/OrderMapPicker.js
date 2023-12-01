1793f256f29399166c1da40a5f3a7537496a702e
Doljinsuren Enkhbayar <doljko927@gmail.com>
2023-12-01 17:32:25 +0800

    fix order map available apps

--- src/components/OrderMapPicker.js
+++ src/components/OrderMapPicker.js
@@ -1,6 +1,6 @@
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import React, { createRef } from 'react';
+import React, { createRef, useEffect, useState } from 'react';
 import { Dimensions, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import ActionSheet from 'react-native-actions-sheet';
 import LaunchNavigator from 'react-native-launch-navigator';
@@ -10,51 +10,35 @@ import { deepGet, getColorCode, translate } from 'utils';
 const windowHeight = Dimensions.get('window').height;
 const dialogHeight = windowHeight / 2;
 
-const OrderMapPicker = ({ title, wrapperStyle, order, buttonStyle }) => {
+const OrderMapPicker = ({ title = 'Select Navigator', wrapperStyle, order, buttonStyle }) => {
     const actionSheetRef = createRef();
     const payload = order.getAttribute('payload');
     const start = deepGet(payload.pickup, 'location.coordinates', []);
     const destination = deepGet(payload.dropoff, 'location.coordinates', []);
+    const [mapProviders, setMapProviders] = useState([]);
 
     title = title ?? translate('components.interface.OrderMapPicker.title');
-    console.log('title', title);
 
     const s = [...start.reverse()];
     const d = [...destination.reverse()];
 
-    const mapProviders = [
-        { id: 1, label: 'Google Maps' },
-        { id: 2, label: 'Apple Maps' },
-        { id: 3, label: 'Waze' },
-    ];
+    useEffect(() => {
+        LaunchNavigator.getAvailableApps().then(setMapProviders);
+    }, []);
 
-    const handleNavigate = async location => {
-        if (location.label === 'Google Maps') {
-            LaunchNavigator.navigate(d, {
-                launchMode: LaunchNavigator.LAUNCH_MODE.TURN_BY_TURN,
-                app: LaunchNavigator.APP.GOOGLE_MAPS,
-                start: s,
-            });
-        } else if (location.label === 'Apple Maps') {
-            LaunchNavigator.navigate(d, {
-                launchMode: LaunchNavigator.LAUNCH_MODE.TURN_BY_TURN,
-                app: LaunchNavigator.APP.APPLE_MAPS,
-                start: s,
-            });
-        } else if (location.label === 'Waze') {
-            LaunchNavigator.navigate(d, {
-                launchMode: LaunchNavigator.LAUNCH_MODE.TURN_BY_TURN,
-                app: LaunchNavigator.APP.WAZE,
-                start: s,
-            });
-        }
+    const handleNavigate = async app => {
+        LaunchNavigator.navigate(d, {
+            launchMode: LaunchNavigator.LAUNCH_MODE.TURN_BY_TURN,
+            app,
+            start: s,
+        });
     };
 
     return (
         <View style={[wrapperStyle]}>
             <TouchableOpacity style={tailwind('flex flex-row items-center px-4 pb-2 mt-1')} onPress={() => actionSheetRef.current?.setModalVisible()}>
-                <View style={tailwind('btn bg-green-900 border border-green-700')}>
-                    <Text style={tailwind('font-semibold text-red-50 text-base')}>Map</Text>
+                <View style={tailwind('btn bg-blue-900 border border-blue-700')}>
+                    <Text style={tailwind('font-semibold text-red-50 text-base')}>Start Navigation</Text>
                 </View>
             </TouchableOpacity>
 
@@ -81,17 +65,19 @@ const OrderMapPicker = ({ title, wrapperStyle, order, buttonStyle }) => {
                         </View>
                     </View>
                     <ScrollView showsHorizontalScrollIndicator={false} showsVerticalScrollIndicator={false}>
-                        {mapProviders.map(location => (
-                            <TouchableOpacity
-                                key={location.id}
-                                onPress={() => {
-                                    handleNavigate(location);
-                                }}>
-                                <View style={tailwind('flex flex-row items-center px-5 py-4 border-b border-gray-900')}>
-                                    <Text style={tailwind('font-semibold text-lg text-gray-100')}>{location.label}</Text>
-                                </View>
-                            </TouchableOpacity>
-                        ))}
+                        {Object.keys(mapProviders).map(provider =>
+                            mapProviders[provider] ? (
+                                <TouchableOpacity
+                                    key={provider}
+                                    onPress={() => {
+                                        handleNavigate(provider);
+                                    }}>
+                                    <View style={tailwind('flex flex-row items-center px-5 py-4 border-b border-gray-900')}>
+                                        <Text style={tailwind('font-semibold text-lg text-gray-100')}>{LaunchNavigator.getAppDisplayName(provider)}</Text>
+                                    </View>
+                                </TouchableOpacity>
+                            ) : null
+                        )}
                         <View style={tailwind('w-full h-40')}></View>
                     </ScrollView>
                 </View>
