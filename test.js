645621f291e87a82fbea686c7f47b16f2db2ecdd
Doljinsuren Enkhbayar <doljko927@gmail.com>
2023-12-22 14:32:16 +0800

    upgraded jdk version

--- test.js
+++
@@ -1,5 +0,0 @@
-import('react-native').then(() => {
-    import('@fleetbase/react-native-mapbox-navigation').then((MapboxNavigation) => {
-        console.log('[MapboxNavigation]', MapboxNavigation);
-    });
-});
