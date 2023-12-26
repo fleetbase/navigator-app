79351e3b9857575804b30bebae32db03b71d3d33
Turtuvshin <toroo.byamba@gmail.com>
2023-12-26 17:07:49 +0800

    bump version to 1.1.8

--- src/hooks/use-fleetbase.js
+++ src/hooks/use-fleetbase.js
@@ -2,7 +2,7 @@ import Fleetbase from '@fleetbase/sdk';
 import config from 'config';
 import { getString, get } from 'utils/Storage';
 let { FLEETBASE_KEY, FLEETBASE_HOST, FLEETBASE_NAMESPACE } = config;
-import Config from 'react-native-config';
+
 const useFleetbase = () => {
     let _DRIVER = get('driver');
     let _FLEETBASE_KEY = getString('_FLEETBASE_KEY');
@@ -12,14 +12,11 @@ const useFleetbase = () => {
         FLEETBASE_KEY = _FLEETBASE_KEY;
     }
 
-    console.log(Config);
-
     if (_FLEETBASE_HOST) {
         FLEETBASE_HOST = _FLEETBASE_HOST;
     }
 
     if (typeof _DRIVER === 'object' && typeof _DRIVER?.token === 'string') {
-        console.log('Driver: ', _DRIVER);
         FLEETBASE_KEY = _DRIVER.token;
     }
 
