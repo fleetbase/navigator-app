7460bf5f07b491f20d7d9a5204715aa234c60781
Turtuvshin <toroo.byamba@gmail.com>
2023-12-26 15:49:56 +0800

    added driver tocken

--- src/hooks/use-fleetbase.js
+++ src/hooks/use-fleetbase.js
@@ -1,9 +1,10 @@
 import Fleetbase from '@fleetbase/sdk';
 import config from 'config';
-import { getString } from 'utils/Storage';
+import { getString, get } from 'utils/Storage';
 let { FLEETBASE_KEY, FLEETBASE_HOST, FLEETBASE_NAMESPACE } = config;
-
+import Config from 'react-native-config';
 const useFleetbase = () => {
+    let _DRIVER = get('driver');
     let _FLEETBASE_KEY = getString('_FLEETBASE_KEY');
     let _FLEETBASE_HOST = getString('_FLEETBASE_HOST');
 
@@ -11,8 +12,15 @@ const useFleetbase = () => {
         FLEETBASE_KEY = _FLEETBASE_KEY;
     }
 
+    console.log(Config);
+
     if (_FLEETBASE_HOST) {
         FLEETBASE_HOST = _FLEETBASE_HOST;
+    }
+
+    if (typeof _DRIVER === 'object' && typeof _DRIVER?.token === 'string') {
+        console.log('Driver: ', _DRIVER);
+        FLEETBASE_KEY = _DRIVER.token;
     }
 
     console.log('_FLEETBASE_HOST', FLEETBASE_HOST);
