780ceeeac3f04893d79b8b10bfc66e4477b8ed7c
Doljinsuren Enkhbayar <doljko927@gmail.com>
2024-02-29 17:59:41 +0800

    updated issuesList

--- src/features/Core/screens/IssuesScreen.js
+++ src/features/Core/screens/IssuesScreen.js
@@ -1,22 +1,21 @@
 import { useDriver, useFleetbase } from 'hooks';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatList, RefreshControl, Text, TouchableOpacity, View } from 'react-native';
 import tailwind from 'tailwind';
 import { getColorCode } from 'utils';
 
-const IssueListScreen = ({ navigation, route }) => {
-    // const { currentIssueListScreen } = route.params;
+const IssuestScreen = () => {
     const [driver] = useDriver();
     const fleetbase = useFleetbase();
-    const [issues, setIssueListScreens] = useState([]);
+    const [issues, setIssueList] = useState([]);
     const [isRefreshing, setIsRefreshing] = useState(false);
 
     const fetchIssues = async () => {
         try {
             const adapter = fleetbase.getAdapter();
-            const issues = await adapter.get('issues');
-            console.log('list------>', issues);
-            return issues;
+            const response = await adapter.get('issues');
+            setIssueList(response);
+            return response;
         } catch (error) {
             console.error('Error fetching  issue:', error);
             return [];
@@ -28,11 +27,11 @@ const IssueListScreen = ({ navigation, route }) => {
     }, []);
 
     const renderItem = ({ item }) => (
-        <TouchableOpacity onPress={() => confirmSwitchIssueListScreen(item.id)}>
+        <TouchableOpacity onPress={() => confirmSwitchIssuestScreen(item.id)}>
             <View style={[tailwind('p-1')]}>
                 <View style={[tailwind('px-4 py-2 flex flex-row items-center rounded-r-md')]}>
                     <Text style={tailwind('text-gray-50 text-base')} numberOfLines={1}>
-                        <Text>{item?.getAttribute('name')}</Text>
+                        <Text>{item.driver_name}</Text>
                     </Text>
                 </View>
             </View>
@@ -43,7 +42,7 @@ const IssueListScreen = ({ navigation, route }) => {
         <View style={tailwind('w-full h-full bg-gray-800 flex-grow')}>
             <View style={tailwind('flex flex-row items-center justify-between p-4 ')}>
                 <View>
-                    <Text style={tailwind('font-bold text-white text-base')}>Issue</Text>
+                    <Text style={tailwind('font-bold text-white text-base')}>Issues</Text>
                 </View>
             </View>
 
@@ -57,4 +56,4 @@ const IssueListScreen = ({ navigation, route }) => {
     );
 };
 
-export default IssueListScreen;
+export default IssuestScreen;
--- src/features/Core/screens/MainScreen.js
+++ src/features/Core/screens/MainScreen.js
@@ -15,7 +15,7 @@ import { tailwind } from 'tailwind';
 import { createNewOrderLocalNotificationObject, getColorCode, listenForOrdersFromSocket, logError } from 'utils';
 import { syncDevice } from 'utils/Auth';
 import { getCurrentLocation, trackDriver } from 'utils/Geo';
-import IssueListScreen from './IssueListScreen';
+import IssuesScreen from './IssuesScreen';
 
 const { addEventListener, removeEventListener } = EventRegister;
 const Tab = createBottomTabNavigator();
@@ -159,7 +159,7 @@ const MainScreen = ({ navigation, route }) => {
                 {/* <Tab.Screen key="routes" name="Routes" component={RoutesScreen} /> */}
                 {/* <Tab.Screen key="schedule" name="Schedule" component={ScheduleStack} /> */}
                 {/* <Tab.Screen key="wallet" name="Wallet" component={WalletScreen} /> */}
-                <Tab.Screen key="issue" name="Issue" component={IssueListScreen} />
+                <Tab.Screen key="issue" name="Issue" component={IssuesScreen} />
                 <Tab.Screen key="account" name="Account" component={AccountStack} />
             </Tab.Navigator>
         </>
