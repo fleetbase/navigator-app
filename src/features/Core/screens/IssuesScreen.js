7628d117b68ea2dddad451d6e570cb5bf9339de1
Doljinsuren Enkhbayar <doljko927@gmail.com>
2024-03-01 11:58:03 +0800

    navigate create issue screen

--- src/features/Core/screens/IssuesScreen.js
+++ src/features/Core/screens/IssuesScreen.js
@@ -2,9 +2,11 @@ import { useDriver, useFleetbase } from 'hooks';
 import React, { useEffect, useState } from 'react';
 import { FlatList, RefreshControl, Text, TouchableOpacity, View } from 'react-native';
 import tailwind from 'tailwind';
-import { getColorCode } from 'utils';
+import { useNavigation } from '@react-navigation/native';
+import { getColorCode, translate } from 'utils';
 
-const IssuestScreen = () => {
+const IssuesScreen = () => {
+    const navigation = useNavigation();
     const [driver] = useDriver();
     const fleetbase = useFleetbase();
     const [issues, setIssueList] = useState([]);
@@ -27,12 +29,14 @@ const IssuestScreen = () => {
     }, []);
 
     const renderItem = ({ item }) => (
-        <TouchableOpacity onPress={() => confirmSwitchIssuestScreen(item.id)}>
-            <View style={[tailwind('p-1')]}>
-                <View style={[tailwind('px-4 py-2 flex flex-row items-center rounded-r-md')]}>
-                    <Text style={tailwind('text-gray-50 text-base')} numberOfLines={1}>
-                        <Text>{item.driver_name}</Text>
-                    </Text>
+        <TouchableOpacity style={tailwind('bg-yellow-900 mb-2')} onPress={() => confirmSwitchIssuestScreen(item.id)}>
+            <View style={[tailwind('border-b border-gray-800 py-3 px-3 flex flex-row items-start justify-between')]}>
+                <View style={[tailwind('flex flex-col')]}>
+                    <Text style={[tailwind('text-white font-semibold mb-1')]}>Title: {item.report}</Text>
+                    <View style={[tailwind('flex flex-row')]}>
+                        <Text style={[tailwind('text-gray-100')]}>drive name: {item.driver_name}</Text>
+                        <Text style={[tailwind('text-gray-100 mx-1')]}>vehicle name: {item.vehicle_name}</Text>
+                    </View>
                 </View>
             </View>
         </TouchableOpacity>
@@ -52,8 +56,21 @@ const IssuestScreen = () => {
                 keyExtractor={item => item.id}
                 renderItem={renderItem}
             />
+            <View style={tailwind('p-4')}>
+                <View style={tailwind('flex flex-row items-center justify-center')}>
+                    <TouchableOpacity
+                        style={tailwind('flex-1')}
+                        onPress={() => {
+                            navigation.navigate('IssueScreen');
+                        }}>
+                        <View style={tailwind('btn bg-gray-900 border border-gray-700')}>
+                            <Text style={tailwind('font-semibold text-gray-50 text-base')}>{translate('Account.AccountScreen.create')}</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     );
 };
 
-export default IssuestScreen;
+export default IssuesScreen;
