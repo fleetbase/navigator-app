f4926dbb64b33b173373761b5b65ed663dc95fe2
Doljinsuren Enkhbayar <doljko927@gmail.com>
2024-04-10 17:45:00 +0800

    fixed messenger container

--- src/features/Core/screens/ChatScreen.js
+++ src/features/Core/screens/ChatScreen.js
@@ -1,14 +1,15 @@
-import { faAngleLeft, faPhone, faVideo,faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { faAngleLeft, faPaperPlane, faPhone, faUpload, faVideo } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { useNavigation } from '@react-navigation/native';
 import React, { useCallback, useEffect, useState } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
-import { Bubble, GiftedChat, Send } from 'react-native-gifted-chat';
+import { Actions, Bubble, GiftedChat, InputToolbar, Send } from 'react-native-gifted-chat';
 import { tailwind } from 'tailwind';
 
 const ChatScreen = () => {
     const navigation = useNavigation();
     const [messages, setMessages] = useState([]);
+    const [text, setText] = useState();
 
     useEffect(() => {
         setMessages([
@@ -42,9 +43,7 @@ const ChatScreen = () => {
     const renderSend = props => {
         return (
             <Send {...props}>
-                <View style={tailwind('rounded-full bg-blue-500 p-2 mb-2 mr-2')}>
-                    <FontAwesomeIcon icon={faPaperPlane} size={14} color="#fff" />
-                </View>
+                <FontAwesomeIcon icon={faPaperPlane} size={20} color="#168AFF" style={tailwind('mr-2')} />
             </Send>
         );
     };
@@ -71,15 +70,43 @@ const ChatScreen = () => {
         return <FontAwesomeIcon name="angle-double-down" size={22} color="#333" />;
     };
 
+    const renderActions = () => (
+        <Actions
+            options={{
+                'Choose From Library': () => {
+                    console.log('Choose From Library');
+                },
+                Cancel: () => {
+                    console.log('Cancel');
+                },
+            }}
+            optionTintColor="#222B45"
+        />
+    );
+
+    const MessengerBarContainer = props => {
+        return <InputToolbar {...props} containerStyle={tailwind('bg-white items-center justify-center mx-2 rounded-lg mb-0')} />;
+    };
+
+    const AdditionalContainer = () => {
+        return (
+            <View style={tailwind('flex flex-row items-center justify-center')}>
+                <TouchableOpacity style={tailwind('p-2 mb-1')}>
+                    <FontAwesomeIcon size={22} icon={faUpload} color="#0084FF" />
+                </TouchableOpacity>
+            </View>
+        );
+    };
+
     return (
-        <View style={tailwind('w-full h-full bg-gray-200 flex-grow p-2')}>
+        <View style={tailwind('w-full h-full bg-gray-200 flex-grow')}>
             <View style={tailwind('flex flex-row ')}>
                 <TouchableOpacity style={tailwind('p-2')} onPress={() => navigation.goBack()}>
                     <FontAwesomeIcon size={25} icon={faAngleLeft} style={tailwind('text-blue-500')} />
                 </TouchableOpacity>
                 <View style={tailwind('flex ml-2')}>
                     <View style={tailwind('flex flex-col ml-2 mt-4')}>
-                        <Text style={tailwind('text-sm text-gray-600 w-64 text-center')}>{'name'}</Text>
+                        <Text style={tailwind('text-sm text-gray-600 w-64 text-center')}>{'Jessica Koel'}</Text>
                     </View>
                 </View>
                 <View style={tailwind('flex flex-col items-center mr-4')}>
@@ -93,7 +120,7 @@ const ChatScreen = () => {
                     </TouchableOpacity>
                 </View>
             </View>
-            <View style={tailwind('flex-1 p-4 mb-4')}>
+            <View style={tailwind('flex-1 p-4')}>
                 <GiftedChat
                     messages={messages}
                     onSend={messages => onSend(messages)}
@@ -103,8 +130,10 @@ const ChatScreen = () => {
                     renderBubble={renderBubble}
                     alwaysShowSend
                     scrollToBottom
-                    renderSend={renderSend}
+                    renderInputToolbar={props => MessengerBarContainer(props)}
+                    renderActions={renderActions}
                     scrollToBottomComponent={scrollToBottomComponent}
+                    renderSend={renderSend}
                 />
             </View>
         </View>
