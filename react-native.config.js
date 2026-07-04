69fbade1e3584421d42ce4c85552c44e3c70210c
Ronald A. Richardson <me@ron.dev>
2026-07-04 19:17:33 +0800

    Upgrade React Native to 0.86 and fix mobile builds

---
+++ react-native.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    dependencies: {
+        '@bam.tech/react-native-image-resizer': {
+            platforms: {
+                android: {
+                    libraryName: null,
+                    componentDescriptors: null,
+                    cmakeListsPath: null,
+                },
+            },
+        },
+        '@react-native-community/blur': {
+            platforms: {
+                android: {
+                    libraryName: null,
+                    componentDescriptors: null,
+                    cmakeListsPath: null,
+                },
+            },
+        },
+    },
+};
